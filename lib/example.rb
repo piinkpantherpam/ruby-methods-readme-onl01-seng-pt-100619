f00c8401ee53d84a10bf6d650e74c573d85941f7
Pam Cairo <pcairo_90@outlook.com>
2019-10-14 04:26:47 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,3 @@
-# Use this space to code along with the readme
-
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -11,3 +9,39 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+
+
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrasee
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase 
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+say_hello_world_ten_times
+
+
+
+say_hello_world_ten_times
