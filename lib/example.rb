daa3817fcca09596880fc32918d3c8d1b72c843a
Pam Cairo <pcairo_90@outlook.com>
2019-10-14 04:40:02 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,3 @@
-# Use this space to code along with the readme
-
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -11,3 +9,58 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+
+
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrasee
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase 
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+say_hello_world_ten_times
+
+
+
+say_hello_world_ten_times
+
+def greeting
+  puts "Hello World"
+end
+
+def greeting
+end
+
+def greeting
+  puts "Hello World"
+end
+
+def greeting
+  puts "Hello World"
+end
+
+greeting
+
+greeting
